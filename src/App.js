7ef1202e73a5ce18f7ba903172910a1a6cc16b08
SviatoslavMJS <toslav@ukr.net>
2020-12-21 16:02:44 +0200

    create modules css

--- src/App.js
+++ src/App.js
@@ -1,36 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './Header';
+import Navbar from './Navbar';
+import Profile from './Profile';
 
 const App = () => {
   return (
     <div className="app-wraper">
-     <header className="header">
-       <img src="https://springboardpr.com/wp-content/uploads/2018/06/green-mountain-coffee-roasters-logo.png"></img>
-     </header>
-     <nav className="nav">
-       <div><a>Profile</a></div>
-       <div><a>Profile</a></div>
-       <div><a>Messages</a></div>
-       <div><a>News</a></div>
-       <div><a>Music</a></div>
-       <div><a>Settings</a></div>
-     </nav>
-     <div className="content">
-        <div>
-          <img src="https://lh3.googleusercontent.com/proxy/joTRg0CdBbFnSEYwIHtfduEq1sA81oP9Ni8Jy5IvCqDZbKJPSmTZUrmc-klqiXyDOJlE6rpOf9GSvv2zmMaT4TnMyPFK60WoH-JKKQoIv6xA1_G5EP6zVRnC3mMpOqYkrFp4r3SSyPkWBe_divKR"></img>
-        </div>
-        <div>avatar+description</div>
-
-        <div>my posts
-          <div>new post</div>
-        </div>
-
-        <div>
-        <div>post 1</div>
-        <div>post 2</div>
-        </div>
-     </div>
-    
+    <Header />
+    <Navbar />
+    <Profile />
     </div>
   );
 }
---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import s from './Header.module.css';
+
+const Header = () => {
+
+return (
+<header className={s.header}>
+<img src="https://f.hubspotusercontent00.net/hubfs/53/1200px-WWF_logo_svg%20(1).png"></img>
+</header>
+)
+}
+
+export default Header;
